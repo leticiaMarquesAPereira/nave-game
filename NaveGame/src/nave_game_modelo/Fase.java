d529791dcc1e28e6e69c05012931cd84db07dd63
leticiaMarquesAPereira <leticia.marques.a.p05@gmail.com>
2024-09-08 21:43:38 -0300

    Classe Player, nave e movimento da nave

--- NaveGame/src/nave_game_modelo/Fase.java
+++ NaveGame/src/nave_game_modelo/Fase.java
@@ -3,22 +3,63 @@ package nave_game_modelo;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Fase extends JPanel {
+public class Fase extends JPanel implements ActionListener {
 	
 	private Image fundo;
+	private Player player;
+	private Timer timer;
 	
 	public Fase() {
+		
+		setFocusable(true);
+		setDoubleBuffered(true);
+		
 		ImageIcon ref = new ImageIcon("res\\\\fundopreto.png");
 		fundo = ref.getImage();
+		
+		player = new Player();
+		player.load();
+		
+		addKeyListener(new TecladoAdapter());
+		
+		timer = new Timer(3, this);
+		timer.start();
 	}
 	
 	public void paint(Graphics g) {
+		
 		Graphics2D graficos = (Graphics2D) g;
 		graficos.drawImage(fundo,0,0,null);
+		graficos.drawImage(player.getImagem(), player.getX(), player.getY(), this);
 		g.dispose();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		player.update();
+		repaint();
+	}
+	
+	private class TecladoAdapter extends KeyAdapter {
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			player.keyPressed(e);
+		}
+		
+		@Override
+		public void keyReleased(KeyEvent e) {
+			player.keyRelease(e);
+		}
 	}
 }
