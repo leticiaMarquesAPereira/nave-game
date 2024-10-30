069c8c9298b5c3cb69bf6b6fd3d6b0690c99750c
leticiaMarquesAPereira <leticia.marques.a.p05@gmail.com>
2024-10-30 02:25:44 -0300

    feat: pontuacao, menu e foto de win

--- NaveGame/src/nave_game_modelo/Fase.java
+++ NaveGame/src/nave_game_modelo/Fase.java
@@ -1,8 +1,12 @@
 package nave_game_modelo;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Font;
+import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -20,17 +25,22 @@ import nave_game_sounds.EfeitosSonoros;
 public class Fase extends JPanel implements ActionListener {
 
 	private Image fundo;
+	private Fase fase;
+	private Menu menu;
 	private Player player;
 	private Timer timer;
 	private List<Inimigo_1> inimigo1;
 	private List<Estrela_1> estrela1;
 	private List<Estrela_2> estrela2;
 	private List<Explosao> explosoes;
+	private int pontos = 0;
+	private boolean venceu = false;
 	private EfeitosSonoros musica;
 	private boolean emJogo, emExplosao;
+	private JButton reiniciar;
 
 	public Fase() {
-
+		
 		setFocusable(true);
 		setDoubleBuffered(true);
 
@@ -49,9 +59,10 @@ public class Fase extends JPanel implements ActionListener {
 		inicializaEstrela();
 		inicializaEstrela2();
 		inicializaExplosoes();
+		inicializaBotaoReiniciar();
 		
 		waitForSeconds();
-		timer = new Timer(5, this);
+		timer = new Timer(0, this);
 		timer.start();
 		
 		emJogo = true;
@@ -98,13 +109,57 @@ public class Fase extends JPanel implements ActionListener {
 
 	}
 	
+	private void inicializaBotaoReiniciar() {
+        reiniciar = new JButton("Jogar Novamente");
+        reiniciar.setBounds(200, 400, 150, 50);
+        reiniciar.setVisible(false);
+        reiniciar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                reiniciarJogo();
+            }
+        });
+        this.setLayout(null);
+        this.add(reiniciar);
+    }
+	
+	private void reiniciarJogo() {
+	    emJogo = true;
+	    player = new Player();
+	    player.load();
+	    inicializaInimigos();
+	    inicializaEstrela();
+	    inicializaEstrela2();
+	    explosoes.clear();  // Limpa as explosões anteriores
+	    if (timer != null) {
+	        timer.stop();
+	    }
+	    timer = new Timer(0, this); // Cria um novo Timer
+	    timer.start();
+	    reiniciar.setVisible(false);
+	    musica.tocarFase(); // Reinicia a música do jogo
+	    repaint();
+	}
 	public void paint(Graphics g) {
 
 		Graphics2D graficos = (Graphics2D) g;
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.drawImage(fundo, 0, 0, getWidth(), getHeight(), this);
+		
+		if(venceu) {
+			ImageIcon voceVenceu = new ImageIcon("res\\youwin.png");
+	        g2d.drawImage(voceVenceu.getImage(), 0, 0, getWidth(), getHeight(), this);
+	        return;
+		}
 		
 		if(emJogo == true) {
 			
 			graficos.drawImage(fundo, 0, 0, null);
+			
+			graficos.setColor(Color.WHITE); 
+		    graficos.setFont(new Font("Arial", Font.BOLD, 20));  
+		    graficos.drawString("Pontos: " + pontos, 420, 40);
 			
 			for (int p = 0; p < estrela1.size(); p++) {
 				Estrela_1 b = estrela1.get(p);
@@ -152,8 +207,10 @@ public class Fase extends JPanel implements ActionListener {
 		}
 		else {
 			
-			ImageIcon fimJogo = new ImageIcon("res\\gameover.png");
-			graficos.drawImage(fimJogo.getImage(), 0, 0, null);
+			if (venceu == false) {
+				ImageIcon fimJogo = new ImageIcon("res\\gameover.png");
+				graficos.drawImage(fimJogo.getImage(), 0, 0, null);
+			}
 		}
 		
 		g.dispose();
@@ -166,65 +223,67 @@ public class Fase extends JPanel implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
-		player.update();
 		
-		for(int p = 0; p < estrela1.size(); p++) {
-			Estrela_1 on = estrela1.get(p);
-			if(on.isVisivel()) {
-				on.update();
-			}
-			else {
-				estrela1.remove(p);
+		if (emJogo) { 
+			player.update();
+			
+			for(int p = 0; p < estrela1.size(); p++) {
+				Estrela_1 on = estrela1.get(p);
+				if(on.isVisivel()) {
+					on.update();
 				}
-		}
-		
-		for(int r = 0; r < estrela2.size(); r++) {
-			Estrela_2 on = estrela2.get(r);
-			if(on.isVisivel()) {
-				on.update();
+				else {
+					estrela1.remove(p);
+					}
 			}
-			else {
-				estrela2.remove(r);
+			
+			for(int r = 0; r < estrela2.size(); r++) {
+				Estrela_2 on = estrela2.get(r);
+				if(on.isVisivel()) {
+					on.update();
 				}
-		}
-
-		List<Tiro> tiros = player.getTiros();
-		for (int i = 0; i < tiros.size(); i++) {
-			Tiro m = tiros.get(i);
-
-			if (m.isVisivel()) {
-				m.update();
-			} else {
-				tiros.remove(i);
-			}
-		}
-		
-		for(int o = 0; o < inimigo1.size(); o++) {
-			Inimigo_1 in = inimigo1.get(o);
-			if(in.isVisivel()) {
-				in.update();
-			}
-			else {
-				int inimigo, inimigo2;
-				inimigo = inimigo1.get(o).getX();
-				inimigo2 = inimigo1.get(o).getY();
-				inimigo1.remove(o);
-				explosoes.add(new Explosao(inimigo, inimigo2));
-			}
-		}
-		
-		for (int q = 0; q < explosoes.size(); q++) {
-			Explosao y = explosoes.get(q);
-			if (y.isVisivel()) {
-				y.update();
-			} else {
-				explosoes.remove(q);
+				else {
+					estrela2.remove(r);
+					}
 			}
 
+			List<Tiro> tiros = player.getTiros();
+			for (int i = 0; i < tiros.size(); i++) {
+				Tiro m = tiros.get(i);
+
+				if (m.isVisivel()) {
+					m.update();
+				} else {
+					tiros.remove(i);
+				}
+			}
+			
+			for(int o = 0; o < inimigo1.size(); o++) {
+				Inimigo_1 in = inimigo1.get(o);
+				if(in.isVisivel()) {
+					in.update();
+				}
+				else {
+					int inimigo, inimigo2;
+					inimigo = inimigo1.get(o).getX();
+					inimigo2 = inimigo1.get(o).getY();
+					inimigo1.remove(o);
+					explosoes.add(new Explosao(inimigo, inimigo2));
+				}
+			}
+			
+			for (int q = 0; q < explosoes.size(); q++) {
+				Explosao y = explosoes.get(q);
+				if (y.isVisivel()) {
+					y.update();
+				} else {
+					explosoes.remove(q);
+				}
+
+			}
+			checarColisoes();
+			repaint();
 		}
-		checarColisoes();
-		repaint();
 	}
 	
 	public void waitForSeconds() {
@@ -237,6 +296,12 @@ public class Fase extends JPanel implements ActionListener {
 			}
 		});
 		timer.start();
+	}
+	
+	public void mostrarBotaoReiniciar() {
+	    reiniciar.setVisible(true);    
+	    this.add(reiniciar, BorderLayout.NORTH);            
+	    this.repaint();               
 	}
 	
 	public void checarColisoes() {
@@ -255,12 +320,16 @@ public class Fase extends JPanel implements ActionListener {
 	            musica.tocarSomDano();
 	            ImageIcon referencia2 = new ImageIcon("res\\explosion1.gif");
 	            
-	              emExplosao = true;
+	            emExplosao = true;
 	            tempInimigo_1.setVisivel(false);
 
 	            if (player.getVida() <= 0) {
-	                player.setVisivel(false);
-	                emJogo = false;
+	                player.setVisivel(false);      
+	                emJogo = false;               
+	                timer.stop();                  
+	                musica.pararFase();            
+
+	                mostrarBotaoReiniciar();    
 	            }
 	        }
 		}
@@ -278,9 +347,30 @@ public class Fase extends JPanel implements ActionListener {
 					ImageIcon referencia2 = new ImageIcon("res\\explosion1.gif");
 					tempInimigo_1.setVisivel(false);
 					tempTiro.setVisivel(false);
+					
+					pontos += 10;
+					
+					if (pontos >= 150) {
+						mostrarMensagemVitoria();
+						venceu = true;
+					}
 				}
 			}
 		}
+	}
+
+	private void mostrarMensagemVitoria() {
+	    emJogo = false;
+	    ImageIcon venceu = new ImageIcon("res\\youwin.png");
+	    Graphics g = getGraphics();
+	    g.drawImage(venceu.getImage(), 0, 0, null);
+	}
+	
+	public void mostrarMenuInicial() {
+	    this.remove(fase); 
+	    this.add(menu); 
+	    this.revalidate();
+	    this.repaint();
 	}
 
 	private class TecladoAdapter extends KeyAdapter {
--- NaveGame/src/nave_game_sounds/EfeitosSonoros.java
+++ NaveGame/src/nave_game_sounds/EfeitosSonoros.java
@@ -1,7 +1,6 @@
 package nave_game_sounds;
 
 import java.net.URL;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -12,68 +11,100 @@ import javax.swing.JPanel;
 
 public class EfeitosSonoros extends JPanel {
 
-	public EfeitosSonoros() {
-		setDoubleBuffered(true);
-	}
-	
-	public void tocarFase() {
-		try {
-			URL soundFile = getClass().getResource("TrilhaSonora.wav");
-			AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
-			DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
-			Clip clip = (Clip) AudioSystem.getLine(info);
-			clip.open(sound);
-			FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-	        volumeControl.setValue(-20.0f);
-			clip.start();
-		} catch (Exception e) {
-			JOptionPane.showInputDialog(this, e);
-		}
-	}
-	
-	public void tocarTiro() {
-		try {
-			URL soundFile = getClass().getResource("SomTiro.wav");
-			AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
-			DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
-			Clip clip = (Clip) AudioSystem.getLine(info);
-			clip.open(sound);
-			FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-	        volumeControl.setValue(-20.0f);
-			clip.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public void tocarSomExplosao() {
-		try {
-			URL soundFile = getClass().getResource("SomDano.wav");
-			AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
-			DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
-			Clip clip = (Clip) AudioSystem.getLine(info);
-			clip.open(sound);
-			FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-	        volumeControl.setValue(-30.0f);
-			clip.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void tocarSomDano() {
-		try {
-			URL soundFile = getClass().getResource("SomExplosao.wav");
-			AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
-			DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
-			Clip clip = (Clip) AudioSystem.getLine(info);
-			clip.open(sound);
-			FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-	        volumeControl.setValue(-30.0f);
-			clip.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private Clip clipFase;
+    private Clip clipMenu;// Atributo para o som da fase
+
+    public EfeitosSonoros() {
+        setDoubleBuffered(true);
+    }
+
+    public void tocarFase() {
+        try {
+            URL soundFile = getClass().getResource("musicaMenu.wav");
+            AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
+            DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+            clipFase = (Clip) AudioSystem.getLine(info); // Armazena o clip
+            clipFase.open(sound);
+            FloatControl volumeControl = (FloatControl) clipFase.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(-05.0f);
+            clipFase.start();
+            clipFase.loop(Clip.LOOP_CONTINUOUSLY); // Para tocar em loop
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, e.getMessage());
+        }
+    }
+
+    public void pararFase() {
+        if (clipFase != null && clipFase.isRunning()) {
+            clipFase.stop();  
+            clipFase.close();  
+        }
+    }
+    
+    public void pararSomMenu() {
+        if (clipMenu != null && clipMenu.isRunning()) {
+        	clipMenu.stop(); 
+        	clipMenu.close();  
+        }
+    }
+
+    public void tocarTiro() {
+        try {
+            URL soundFile = getClass().getResource("SomTiro.wav");
+            AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
+            DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+            Clip clip = (Clip) AudioSystem.getLine(info);
+            clip.open(sound);
+            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(-20.0f);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tocarSomExplosao() {
+        try {
+            URL soundFile = getClass().getResource("SomDano.wav");
+            AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
+            DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+            Clip clip = (Clip) AudioSystem.getLine(info);
+            clip.open(sound);
+            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(-30.0f);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tocarSomDano() {
+        try {
+            URL soundFile = getClass().getResource("SomExplosao.wav");
+            AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
+            DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+            Clip clip = (Clip) AudioSystem.getLine(info);
+            clip.open(sound);
+            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(-30.0f);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void tocarSomMenu() {
+        try {
+            URL soundFile = getClass().getResource("TrilhaSonora.wav");
+            AudioInputStream sound = AudioSystem.getAudioInputStream(soundFile);
+            DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+            clipMenu = (Clip) AudioSystem.getLine(info);
+            clipMenu.open(sound);
+            FloatControl volumeControl = (FloatControl) clipMenu.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(-30.0f);
+            clipMenu.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
